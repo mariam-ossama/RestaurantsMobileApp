a7668189dfa8e0a32ea5a97433d2e39c86134e91
mariam-ossama <mariamosama772@gmail.com>
2024-06-29 11:55:40 +0300

    Commit

---
+++ ema_project/SQLQuery1.sql
@@ -0,0 +1,14 @@
+
+
+insert into Products(RestaurantId,Name) values(12,'Fries')
+insert into Products(RestaurantId,Name) values(13,'Fries')
+insert into Products(RestaurantId,Name) values(14,'Fries')
+insert into Products(RestaurantId,Name) values(17,'Fries')
+insert into Products(RestaurantId,Name) values(16,'7 Up')
+insert into Products(RestaurantId,Name) values(12,'7 UP')
+
+insert into Restaurants(Name,Address,Latitude,Longitude) values ('Maryland House Travel Plaza','MD',39.497467,-76.232224)
+insert into Restaurants(Name,Address,Latitude,Longitude) values ('The Oasis On Lake Travis','TX',30.405561,	-97.874191)
+insert into Restaurants(Name,Address,Latitude,Longitude) values ('Founding Farmers','DC',38.900322,-77.044571)
+insert into Restaurants(Name,Address,Latitude,Longitude) values ('KFC','Maadi - Cairo',29.9538,31.5370)
+insert into Restaurants(Name,Address,Latitude,Longitude) values ('Honey Cup Coffee and Kitchen','CA',34.168907,-119.229004)
